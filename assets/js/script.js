5dfaaafa7c2ff9aa15b16c49b27817bc0f43bc8c
MOHAMED HAJJI <pytaek1@gmail.com>
2022-01-04 00:41:52 +0100

    firstd

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,60 @@ let songs = [
         lyrics: '',
     },
     {
+        name: 'High(deat.Aleesia)',
+        src: 'assets/songs/song10.mp3',
+        cover: 'assets/images/song10.jpg',
+        artist: 'JPB',
+        lyrics: '',
+    },
+    {
+        name: 'Long Night pt.||',
+        src: 'assets/songs/song11.mp3',
+        cover: 'assets/images/song11.jpg',
+        artist: 'JPB',
+        lyrics:
+            '2.8 | We gon turn the crib into a club like\n' +
+            "5.7 | Hennessy and Buddah screamin' thug life'\n" +
+            " 8 | Get it baby, you know I'm the plug, right'\n" +
+            " 10 | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            " 11 | Whoa'\n" +
+            " 13 | Everything I touch turned gold'\n" +
+            " 15 | Need no introduction, 'cause they know briggin more''\n" +
+            " 17.3 | I hit the lotto, my motto roll up Gelato and Throttle'\n" +
+            " 20 | A couple models to caudal who fuckin' brought out the bottles'\n" +
+            " 22 | I'm goin' ham'\n" +
+            " 23 | This ain't even luck, all planned'\n" +
+            " 25 | Y'all know who the fucks I am'\n" +
+            " 27 | Burnin' bucks mo bands'\n" +
+            " 29 | Always been the M.O. since the start of this'\n" +
+            " 30.5 | And now I'm on a mission like I'm Optimus, aye'\n" +
+            "  33.5| We gon turn the crib into a club like'\n" +
+            "  36 | Hennessy and Buddah screamin' thug life'\n" +
+            "  39 | Get it baby, you know I'm the plug, right'\n" +
+            "  40 | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            "  51 | Got a feelin' that it's gonna be a long night'\n" +
+            "  62 | Yеah, never an averagе night'\n" +
+            "  64 | Two, four hours up in a day I gotta make it right'\n" +
+            "  67 | 18 of those I'm in a cave I gotta make some light'\n" +
+            "  69 | Make it hype, even if we breakin' night'\n" +
+            "72 | And even if things go left we gotta make 'em right'\n" +
+            "74 |  Chillin' with M.I.M.E we triple up aye 'onna double UP'\n" +
+            "76.4 | Triple dub, came from the basement we only goin' up'\n" +
+            "79 | They lovin' us, up in the booth I'm getting dizzy,'\n" +
+            "80  | but aye, the cup is up'\n" +
+            "82  | Toast to my people, that's on the come up, aye'\n" +
+            "84  | You lit with us, in order by the trigger buzz'\n" +
+            "86  | Lookin' at my watches a spirit bomb both hands are goin' up'\n" +
+            "88.5  | You comin' coast to coast with us'\n" +
+            "90  | See the flows are flowin' over, flowin' yeah'\n" +
+            "92  | The cup it run it over title wave up in the ocean,ah'\n" +
+            "95  | We gon turn the crib into a club like'\n" +
+            "97  | Hennessy and Buddah screamin' thug life'\n" +
+            "100  | Get it baby, you know I'm the plug, right'\n" +
+            "102  | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            "112  | Got a feelin' that it's gonna be a long night'\n",
+    },
+    {
         name: 'Royalty(ft.Neoni)',
         src: 'assets/songs/song9.mp3',
         cover: 'assets/images/song9.jpg',
@@ -154,6 +208,7 @@ let albums = [
 function pad(d) {
     return d < 10 ? '0' + d.toString() : d.toString();
 }
+var type = 'normal';
 $(document).ready(function () {
     songs.map(function (song, i) {
         var temp =
@@ -161,9 +216,9 @@ $(document).ready(function () {
             i +
             '"> <img src="assets/images/equalizer.gif" alt="" class="equalizer_gif" /> <div class="song_num">' +
             pad(i + 1) +
-            '</div> <img src="' +
+            '</div> <div style="position:relative;"><img src="assets/images/play.png" class="play_hover" alt=""><img src="' +
             song.cover +
-            '" class="song_img" /> <div class="song_info"> <div class="song_name">' +
+            '" class="song_img" /></div> <div class="song_info"> <div class="song_name">' +
             song.name +
             '</div> <div class="song_artist">' +
             song.artist +
@@ -202,6 +257,60 @@ var updateTrack = setInterval(function () {
     var time = formatTime(audio.currentTime);
     $('.time_start').html(time);
     $('.volume_bar').css('width', '' + volume_size * 150 + 'px');
+    if (
+        audioPlayer.currentTime == audioPlayer.duration &&
+        audioPlayer.duration != 0
+    ) {
+        var id = parseInt($('.song[selected=selected]').attr('data-id'));
+        if (type == 'normal') {
+            if (id == songs.length - 1) {
+                var idd = 0;
+            } else {
+                var idd = id + 1;
+            }
+        } else if (type == 'shuffle') {
+            var idd = Math.floor(Math.random() * (songs.length - 1));
+            if (idd == id) {
+                var idd = Math.floor(Math.random() * (songs.length - 1));
+            }
+        } else {
+            var idd = id;
+        }
+
+        audioPlayer.setAttribute('src', songs[idd].src);
+        audioPlayer.play();
+        $('.song[selected=selected]')
+            .removeAttr('selected')
+            .removeClass('song_active');
+        $('.song[data-id=' + idd + ']')
+            .attr('selected', 'true')
+            .addClass('song_active');
+        $('.song_player').find('.song_img').attr('src', songs[idd].cover);
+        $('.song_player').find('.song_name').text(songs[idd].name);
+        $('.song_player').find('.song_artist').text(songs[idd].artist);
+        $('.song[data-id=' + id + ']')
+            .find('.song_num')
+            .show();
+        $('.song[data-id=' + id + ']')
+            .find('.equalizer_gif')
+            .hide();
+        $('.song[data-id=' + idd + ']')
+            .find('.song_num')
+            .hide();
+        $('.song[data-id=' + idd + ']')
+            .find('.equalizer_gif')
+            .show();
+        $('.right').css(
+            'background-image',
+            'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),url(' +
+                songs[idd].cover +
+                ')'
+        );
+        $('.right_song_img').attr('src', '' + songs[idd].cover + '');
+        $('.right_song_name').text('' + songs[idd].name + '');
+        $('.right_song_artist').text('' + songs[idd].artist + '');
+        lyrics(idd);
+    }
 }, 500);
 $(document).on('click', '#play', function () {
     audio.play();
@@ -233,6 +342,10 @@ vol.addEventListener('click', function (e) {
     } catch (err) {
         // Fail silently but show in F12 developer tools console
         if (window.console && console.error('Error:' + err));
+    }
+    if ($('#mute_icon').is(':visible')) {
+        $('#mute_icon').hide();
+        $('#volume_icon').show();
     }
 });
 
@@ -271,33 +384,7 @@ $(document).on('click', '.song', function () {
     $('.right_song_img').attr('src', '' + songs[id].cover + '');
     $('.right_song_name').text('' + songs[id].name + '');
     $('.right_song_artist').text('' + songs[id].artist + '');
-
-    var lyrics = songs[id].lyrics;
-
-    if (lyrics != '') {
-        const lyrics_div = document.querySelector('.lyrics');
-        const lines = lyrics.trim().split('\n');
-
-        lyrics_div.style.display = 'block';
-        lyrics_div.innerText = '';
-
-        let syncData = [];
-
-        lines.map((line, index) => {
-            const [time, text] = line.trim().split('|');
-            syncData.push({
-                start: time.trim(),
-                text: text.trim(),
-            });
-        });
-
-        audioPlayer.addEventListener('timeupdate', () => {
-            syncData.forEach((item) => {
-                if (audioPlayer.currentTime >= item.start)
-                    lyrics_div.innerText = item.text;
-            });
-        });
-    }
+    lyrics(id);
 });
 function formatTime(seconds) {
     minutes = Math.floor(seconds / 60);
@@ -360,54 +447,7 @@ $(document).on('click', '.play_player', function () {
     $('.song[selected=selected]').find('.equalizer_gif').show();
     $('.bottom_equalizer_gif').show();
 });
-/*
-search.addEventListener('keyup', function () {
-    var searchTerm = search.value.toLowerCase();
-    if (searchTerm == '') {
-        container.innerHTML = '';
-        initialize_songs();
-    } else {
-        var results = songs
-            .map(function (song, i) {
-                var { name, artist } = song;
-                if (
-                    name.toLowerCase().includes(searchTerm) == true ||
-                    artist.toLowerCase().includes(searchTerm) == true
-                ) {
-                    return song;
-                }
-            })
-            .filter(Boolean);
 
-        if (results.length > 0) {
-            results.map(function (song, i) {
-                container.innerHTML =
-                    '<div class="song" data-src=' +
-                    song.src +
-                    '><img class="song_img" src=' +
-                    song.cover +
-                    ' alt="" /><div class="col"><div class="song_name">' +
-                    song.name +
-                    '</div><div class="song_artist">' +
-                    song.artist +
-                    '</div></div></div>';
-            });
-            var song = document.querySelectorAll('.song');
-            for (let i = 0; i < song.length; i++) {
-                song[i].addEventListener('click', function (e) {
-                    var img = song[i]
-                        .querySelector('.song_img')
-                        .getAttribute('src');
-                    var src = song[i].getAttribute('data-src');
-                    audioPlayer.setAttribute('src', src);
-                    audioPlayer.play();
-                });
-            }
-        }
-    }
-});
-*/
-var results = [];
 $(document).on('keyup', '.song_input_search', function (e) {
     var searchTerm = $(this).val();
     if (searchTerm == '') {
@@ -428,18 +468,18 @@ $(document).on('keyup', '.song_input_search', function (e) {
             $('.left_songs_list').append(temp);
         });
     } else {
-        results = songs.map(function (song, i) {
-            var { name, artist } = song;
-            if (
-                name.toLowerCase().includes(searchTerm) == true ||
-                artist.toLowerCase().includes(searchTerm) == true
-            ) {
-                return song;
-            }
-        });
-        console.log(results.length);
+        var results = songs
+            .map(function (song, i) {
+                var { name, artist } = song;
+                if (
+                    name.toLowerCase().includes(searchTerm) == true ||
+                    artist.toLowerCase().includes(searchTerm) == true
+                ) {
+                    return song;
+                }
+            })
+            .filter(Boolean);
         if (results.length > 0) {
-            console.log(results);
             results.map(function (song, i) {
                 console.log(song);
                 var temp =
@@ -459,4 +499,157 @@ $(document).on('keyup', '.song_input_search', function (e) {
             });
         }
     }
+});
+var saved_volume = 1;
+$(document).on('click', '#volume_icon', function (e) {
+    saved_volume = audioPlayer.volume;
+    audioPlayer.volume = 0;
+    $(this).hide();
+    $('#mute_icon').show();
+});
+$(document).on('click', '#mute_icon', function (e) {
+    audioPlayer.volume = saved_volume;
+    $(this).hide();
+    $('#volume_icon').show();
+});
+$(document).on('click', '#next_song', function (e) {
+    var id = parseInt($('.song[selected=selected]').attr('data-id'));
+    if (type == 'normal') {
+        if (id == songs.length - 1) {
+            var idd = 0;
+        } else {
+            var idd = id + 1;
+        }
+    } else if (type == 'shuffle') {
+        var idd = Math.floor(Math.random() * songs.length);
+        if (idd == id) {
+            var idd = Math.floor(Math.random() * (songs.length - 1));
+        }
+    } else {
+        var idd = id;
+    }
+
+    audioPlayer.setAttribute('src', songs[idd].src);
+    audioPlayer.play();
+    $('.song[selected=selected]')
+        .removeAttr('selected')
+        .removeClass('song_active');
+    $('.song[data-id=' + idd + ']')
+        .attr('selected', 'true')
+        .addClass('song_active');
+    $('.song_player').find('.song_img').attr('src', songs[idd].cover);
+    $('.song_player').find('.song_name').text(songs[idd].name);
+    $('.song_player').find('.song_artist').text(songs[idd].artist);
+    $('.song[data-id=' + id + ']')
+        .find('.song_num')
+        .show();
+    $('.song[data-id=' + id + ']')
+        .find('.equalizer_gif')
+        .hide();
+    $('.song[data-id=' + idd + ']')
+        .find('.song_num')
+        .hide();
+    $('.song[data-id=' + idd + ']')
+        .find('.equalizer_gif')
+        .show();
+    $('.right').css(
+        'background-image',
+        'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),url(' +
+            songs[idd].cover +
+            ')'
+    );
+    $('.right_song_img').attr('src', '' + songs[idd].cover + '');
+    $('.right_song_name').text('' + songs[idd].name + '');
+    $('.right_song_artist').text('' + songs[idd].artist + '');
+    lyrics(idd);
+});
+$(document).on('click', '#prev_song', function (e) {
+    var id = parseInt($('.song[selected=selected]').attr('data-id'));
+    if (id == 0) {
+        var idd = songs.length - 1;
+    } else {
+        var idd = id - 1;
+    }
+
+    audioPlayer.setAttribute('src', songs[idd].src);
+    audioPlayer.play();
+    $('.song[selected=selected]')
+        .removeAttr('selected')
+        .removeClass('song_active');
+    $('.song[data-id=' + idd + ']')
+        .attr('selected', 'true')
+        .addClass('song_active');
+    $('.song_player').find('.song_img').attr('src', songs[idd].cover);
+    $('.song_player').find('.song_name').text(songs[idd].name);
+    $('.song_player').find('.song_artist').text(songs[idd].artist);
+    $('.song[data-id=' + id + ']')
+        .find('.song_num')
+        .show();
+    $('.song[data-id=' + id + ']')
+        .find('.equalizer_gif')
+        .hide();
+    $('.song[data-id=' + idd + ']')
+        .find('.song_num')
+        .hide();
+    $('.song[data-id=' + idd + ']')
+        .find('.equalizer_gif')
+        .show();
+    $('.right').css(
+        'background-image',
+        'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)),url(' +
+            songs[idd].cover +
+            ')'
+    );
+    $('.right_song_img').attr('src', '' + songs[idd].cover + '');
+    $('.right_song_name').text('' + songs[idd].name + '');
+    $('.right_song_artist').text('' + songs[idd].artist + '');
+    lyrics(idd);
+});
+function lyrics(id) {
+    var lyrics = songs[id].lyrics;
+
+    if (lyrics != '') {
+        const lyrics_div = document.querySelector('.lyrics');
+        const lines = lyrics.trim().split('\n');
+
+        lyrics_div.style.display = 'block';
+        lyrics_div.innerText = '';
+
+        let syncData = [];
+
+        lines.map((line, index) => {
+            const [time, text] = line.trim().split('|');
+            syncData.push({
+                start: time.trim(),
+                text: text.trim(),
+            });
+        });
+
+        audioPlayer.addEventListener('timeupdate', () => {
+            syncData.forEach((item) => {
+                if (audioPlayer.currentTime >= item.start)
+                    lyrics_div.innerText = item.text;
+            });
+        });
+    } else {
+        $('.lyrics').hide();
+    }
+}
+$(document).on('click', '#replaySong', function () {
+    audioPlayer.currentTime = 0;
+});
+$(document).on('click', '#order', function () {
+    type = 'shuffle';
+    $(this).hide();
+    $('#shuffle').show();
+});
+$(document).on('click', '#shuffle', function () {
+    type = 'replaySameSong';
+    $(this).hide();
+    $('#replaySameSong').show();
+});
+$(document).on('click', '#replaySameSong', function () {
+    type = 'normal';
+    $(this).hide();
+    $('#order').show();
 });
